09d90d7343d9c58a4deab661ebe6fc68341c4465
Shubham Pandey <shubhampandey1964@gmail.com>
2023-11-27 16:09:20 +0530

    added:connect to drive feature

---
+++ src/components/Buttons/ConnectToDriveBtn.jsx
@@ -0,0 +1,104 @@
+import { Button } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+
+import {style} from './Button';
+const ConnectToDriveBtn = () => {
+  const [exchangeCode, setExchangeCode] = useState('')
+  const [accessToken, setAccessToken] = useState('')
+  // useEffect(() => {
+  //   // Check if there is a code parameter in the URL
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   const code = urlParams.get('code');
+
+  //   console.log(code);
+    
+  //   const exchangeCodeForToken = async (code) => {
+  //     const clientId = '140717059895-8j0l40lgsr3t6g9trmra8flmtuviqrp5.apps.googleusercontent.com';
+  //     const clientSecret = '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';
+  //     const redirectUri = 'http://localhost:3000';
+      
+  //     const tokenUrl = 'https://accounts.google.com/o/oauth2/token';
+      
+  //     // Request an access token
+  //     const response = await fetch(tokenUrl, {
+  //       method: 'POST',
+  //       headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //     body: `code=${code}&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&grant_type=authorization_code`,
+  //   });
+
+  //   const data = await response.json();
+  //   console.log(data);
+  //   // Store the access token securely for future API requests
+  //   console.log('Access Token:', data.access_token);
+  // };
+  // if (code) {
+  //   // Call a function to exchange the code for an access token
+  //   setExchangeCode(code)
+  //   exchangeCodeForToken(code);
+  // }
+  // }, [exchangeCode])
+  
+  const handleButtonClick = () => {
+    const clientId = '140717059895-8j0l40lgsr3t6g9trmra8flmtuviqrp5.apps.googleusercontent.com';
+    const redirectUri = 'http://localhost:3000';
+    const scope = 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly';
+
+    
+
+    const authEndpoint = 'https://accounts.google.com/o/oauth2/auth';
+    const authorizationUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
+
+    // Open the authorization URL in a new window
+    window.location.href = authorizationUrl
+    // const authWindow = window.open(authorizationUrl, '_blank', 'width=600,height=600');
+
+    // Listen for changes in the new window's location
+    // const checkForToken = () => {
+    //   const hash = authWindow.location.hash;
+    //   if (hash.includes('access_token')) {
+    //     const token = hash.match(/access_token=([^&]+)/)[1];
+    //     console.log(token);
+    //     setAccessToken(token);
+    //     localStorage.setItem('accessToken',accessToken)
+    //     authWindow.close();
+    //   } else {
+    //     setTimeout(checkForToken, 1000);
+    //   }
+    // };
+
+    // checkForToken();
+    // // Open a new window for the user to authorize the application
+    // const authWindow = window.open(authorizationUrl, '_blank');
+
+    // Handle the redirect URL after authorization
+    const handleRedirect = () => {
+
+      const hashParams = new URLSearchParams(window.location.hash.substring(1));
+      const accessToken = hashParams.get('access_token');
+      console.log(accessToken);
+      if (accessToken) {
+        localStorage.setItem('accessToken',accessToken)
+        setAccessToken(accessToken);
+      } else {
+        console.error('Error retrieving access token');
+      }
+      
+    };
+
+
+    handleRedirect()
+
+  };
+ 
+  return (
+    <>
+      <Button sx={style}  component="label" onClick={handleButtonClick} >
+        Connect to Drive
+      </Button>
+    </>
+  )
+}
+
+export default ConnectToDriveBtn
---
+++ src/components/ImportOptionsModal.jsx
@@ -0,0 +1,131 @@
+import { Button, Menu, MenuItem } from '@mui/material';
+import React, { useRef, useState } from 'react'
+import { ImportFamilyBtn } from './Buttons/ImportFamilyBtn';
+import { useTreeState } from '../contexts';
+
+const style={
+  //backgroundColor:'#e7f5ec',
+  // color:'black',
+  border:'0px',
+  marginTop:'0px',
+  marginBottom:'0',
+  paddingTop:'0px',
+  fontSize:14,
+  
+}
+const ImportOptionsModal = ({setOpenModal,openModal}) => {
+  const [isUploading, setIsUploading] = useState(false)
+  const [data,setData] = useTreeState()
+  const [files,setFiles] = useState()
+
+  const inputRef = useRef(null)
+  
+  const handleFileUpload = (file) => {
+    const fr = new FileReader()
+    fr.onload = function (e) {
+      try {
+        const result = JSON.parse(e.target.result)
+        const formatted = JSON.stringify(result, null, 2)
+        result.value = formatted
+
+        // console.log(result)
+        setData(result)
+        // setSelectedNode(result)
+      } catch (error) {
+        // console.error('Error parsing JSON:', error)
+        alert('Invalid JSON file')
+      } finally {
+        setIsUploading(false)
+      }
+    };
+
+    fr.readAsText(file)
+  }
+
+  const handleUploadClick = () => {
+    inputRef.current.click()
+  }
+
+  const handleInputChange = (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      setIsUploading(true)
+      handleFileUpload(file)
+    }
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
+    if (file) {
+      setIsUploading(true)
+      handleFileUpload(file)
+    }
+  }
+
+  const handleReadFile = async () => {
+    const accessToken = localStorage.getItem('accessToken')
+    const folderId = process.env.REACT_APP_FOLDER_ID
+    const apiKey = process.env.REACT_APP_API_KEY
+    console.log(accessToken);
+    if (!accessToken) {
+      console.error('Access token not available. Please authenticate first.');
+      return;
+    }
+
+    const apiUrl = 'https://www.googleapis.com/drive/v3/files'; // Replace FILE_ID with the actual file ID
+    const response = await fetch(`${apiUrl}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('File data:', data.files);
+      if (data.files) {
+        setFiles(data.files.filter(file => file.mimeType === 'application/json'));
+      }
+    } else {
+      console.error('Failed to read file:', response.status, response.statusText);
+    }
+  };
+
+  return (
+    <Menu
+      sx={{
+        marginLeft:`40px`,
+        paddingTop:'0',
+        
+      }}
+      
+      id="basic-menu"
+      // anchorEl={anchorEl}
+      open={openModal}
+      onClose={() => setOpenModal(false)}
+      // onClose={handleClose}
+      MenuListProps={{
+        'aria-labelledby': 'basic-button',
+      }}
+      onDrop={handleDrop}
+        onDragOver={(e) => e.preventDefault()}
+    >
+        <input
+          id="upload"
+          ref={inputRef}
+          type="file"
+          style={{ display: 'none' }}
+          onChange={handleInputChange}
+        />
+      <MenuItem sx={style} onClick={handleUploadClick} >
+        Local Disk
+      </MenuItem>
+      <MenuItem sx={style} onClick={handleReadFile} >Import From Google Drive</MenuItem>
+     
+    </Menu>
+  )
+}
+
+export default ImportOptionsModal
